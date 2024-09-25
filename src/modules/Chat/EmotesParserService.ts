85d532447875d0546239385ad3366d6e5b9d7532
Noel Lopez <nlopez.publi@gmail.com>
2024-09-25 13:44:59 +0200

    feat: emote parsing

--- src/modules/Chat/EmotesParserService.ts
+++ src/modules/Chat/EmotesParserService.ts
@@ -5,6 +5,47 @@ export const EMOTE_URL_TEMPLATE = 'https://static-cdn.jtvnw.net/emoticons/v2/%id
 
 export class EmotesParserService implements EmotesParserInterface {
     parseMessageWithEmotes(message: string, emoteOccurrences: string): MessageSegment[] {
-        return [{ type: 'text', content: message }];
+        if (!emoteOccurrences) {
+            return [{ type: 'text', content: message }];
+        }
+
+        interface Emote {
+            id: string
+            start: number
+            end: number
+        }
+
+        const messageSegments: MessageSegment[] = [];
+        const emotes: Emote[] = [];
+
+        emoteOccurrences.split('/').forEach((emoteOccurrence) => {
+            const [id, positions] = emoteOccurrence.split(':');
+            positions.split(',').forEach((position) => {
+                const [start, end] = position.split('-').map(Number);
+                emotes.push({ id, start, end });
+            });
+        });
+
+        emotes.sort((a, b) => a.start - b.start);
+
+        let lastEnd = 0;
+
+        emotes.forEach(emote => {
+            if (emote.start > lastEnd) {
+                messageSegments.push({ type: 'text', content: message.slice(lastEnd, emote.start) });
+            }
+
+            const emoteUrl = EMOTE_URL_TEMPLATE.replace('%id', emote.id);
+
+            messageSegments.push({ type: 'emote', content: emoteUrl });
+
+            lastEnd = emote.end + 1;
+        })
+
+        if (lastEnd < message.length) {
+            messageSegments.push({ type: 'text', content: message.slice(lastEnd) });
+        }
+
+        return messageSegments;
     }
 }
